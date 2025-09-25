51facc7baea80d804dd5f4255dc1fefc4d9368b5
Mohamed <cheikhimohamed51@gmail.com>
2025-09-25 11:58:30 +0100

    deleted unnecessary files

--- offerTrack/src/main/java/com/offerTrack/offerTrack/repository/MontantConcurrentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.offerTrack.offerTrack.repository;
-
-import com.offerTrack.offerTrack.model.MontantConcurrent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MontantConcurrentRepository extends JpaRepository<MontantConcurrent,String> {
-}
